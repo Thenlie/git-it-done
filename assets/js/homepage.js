7e8512cb58d4751add317daece7f4c33f789019b
Leithen <leithen113@gmail.com>
2021-10-09 10:22:12 -0600

    Loading user data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,20 @@
+// var getUserRepos = function() {
+//     fetch("https://api.github.com/users/octocat/repos").then(function(repsonse) {
+//         response.json().then(function(data) {
+//             console.log(data);
+//         });
+//     });
+
+// };
+
+// getUserRepos();
+
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+}
 
 getUserRepos();
