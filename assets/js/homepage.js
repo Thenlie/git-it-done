ef2c665e7b0e449f7318353733956d22e9da2e82
Leithen <leithen113@gmail.com>
2021-10-09 13:14:16 -0600

    Starting to work on getting user input

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -7,4 +7,8 @@ var getUserRepos = function(user) {
     });
 };
 
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+
 getUserRepos();
