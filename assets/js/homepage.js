f8ab62ecefdccfa077bb887fa7271b48147e91bc
Leithen <leithen113@gmail.com>
2021-10-12 11:48:55 -0600

    Added buttons to filter by language:

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var getUserRepos = function(user) { //make API call to get repo
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -69,4 +70,27 @@ var displayRepos = function(repos, searchTerm) { //display repo on page
     }
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data.items, language);
+                });
+            } else {
+                alert("ErrorL GitHub User Not Found");
+            }
+        });
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language")
+    if (language) {
+        getFeaturedRepos(language)
+        repoContainerEl.textContent = "";
+    }
+}
+
 userFormEl.addEventListener("submit", formSubmitHnadler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
